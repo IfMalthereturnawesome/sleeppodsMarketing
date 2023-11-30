0f9782dc6399c61c4baec7bea124364e9954e728
ChineseThunder <simo09j9@stud.kea.dk>
2023-11-30 14:14:52 +0100

    features done

--- app/posthog.js
+++ app/posthog.js
@@ -1,9 +1,9 @@
 // app/posthog.js
-import { PostHog } from 'posthog-node'
+import { PostHog } from "posthog-node";
 
 export default function PostHogClient() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  })
-  return posthogClient
+  });
+  return posthogClient;
 }
