0cc2e0530bfb878e0e9bda8235596b64246bc59d
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-12-05 14:16:28 +0100

    Add ScrollToTop

--- components/ppg/features-02.tsx
+++ components/ppg/features-02.tsx
@@ -7,6 +7,9 @@ import FeatureImg02 from "@/public/images/feature-image-02.png";
 import FeatureImg03 from "@/public/images/feature-image-03.png";
 
 export default function Features02PPG() {
+  const scrollToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
   return (
     <section className="relative">
       {/* Particles animation */}
@@ -124,17 +127,17 @@ export default function Features02PPG() {
                           </div>
                         </div>
                         <div>
-                          <a
+                          <button
                             className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none"
-                            href="#0"
+                            onClick={scrollToTop}
                           >
                             <span className="relative inline-flex items-center">
-                              Learn more{" "}
+                               Pre-order now
                               <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
                                 -&gt;
                               </span>
                             </span>
-                          </a>
+                          </button>
                         </div>
                       </div>
                       {/* Image */}
