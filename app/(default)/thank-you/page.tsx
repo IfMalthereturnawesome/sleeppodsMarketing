7980683b4757d4ccb7f71c53893bdc3fed1bf70f
ChineseThunder <simo09j9@stud.kea.dk>
2023-12-13 14:26:40 +0100

    thank you page mobile update

--- app/(default)/thank-you/page.tsx
+++ app/(default)/thank-you/page.tsx
@@ -21,51 +21,60 @@ const ThankYouPage = () => {
 
   return (
     <Container>
-      <div className="min-h-[90vh] bg-gray-100 flex flex-col items-center justify-center px-4">
-        <div className="max-w-xl text-center text-slate-900">
-          <h1 className="text-2xl md:text-3xl font-semibold mb-5">
-            Thank You for Joining the Waitlist!
-          </h1>
-          <p className="text-slate-900 mb-4 text-sm md:text-xs">
-            {email ? (
-              <>
-                A confirmation email has been sent to <strong>{email}</strong>.
-                If you don’t see it soon, please check your spam folder.
-              </>
-            ) : (
-              "Please check your email for a confirmation message."
-            )}
-          </p>
-          <p className="mb-4 text-sm">
-            We've received your request for{" "}
-            <strong>early access to SleepPods</strong>. You're now on our list,
-            and we'll keep you <strong>updated on our progress</strong>.
-          </p>
-          <p className="mb-4 text-sm">
-            In the meantime, why not check out Eight Athletics'{" "}
-            <strong>revolutionary Sleep Mask One</strong>, designed for{" "}
-            <strong>maximum comfort</strong> and <strong>total blackout</strong>
-            .
-          </p>
-          <div className="mb-5">
-            <Image
-              src={"/images/image5.jpg"}
-              width="300"
-              height="300"
-              alt="Sleep Mask One from Eight Athletics"
-              className="rounded-lg object-contain mx-auto shadow-black drop-shadow-sm"
-            />
+      <div className="mt-10 md:mt-0 bg-gray-100 flex flex-col items-center justify-center px-4 rounded-2xl">
+        <div className="md:mx-20 text-center text-slate-900 flex flex-col md:gap-10 md:pt-10">
+          <div className="flex flex-col py-10 md:py-0 gap-4">
+            <h1 className="text-2xl md:text-6xl font-semibold ">
+              Thank You for Joining the Waitlist!
+            </h1>
+            <p className="text-slate-900  text-xs md:text-xl">
+              {email ? (
+                <>
+                  A confirmation email has been sent to <strong>{email}</strong>
+                  . If you don’t see it soon, please check your spam folder.
+                </>
+              ) : (
+                "Please check your email for a confirmation message."
+              )}
+            </p>
+            <p className=" text-sm md:text-2xl">
+              We've received your request for{" "}
+              <strong>early access to SleepPods</strong>. You're now on our
+              list, and we'll keep you <strong>updated on our progress</strong>.
+            </p>
+            <p className=" text-sm md:text-2xl">
+              In the meantime, why not check out Eight Athletics'{" "}
+              <strong>revolutionary Sleep Mask One</strong>, designed for{" "}
+              <strong>maximum comfort</strong> and{" "}
+              <strong>total blackout</strong>.
+            </p>
           </div>
-          <a
-            href="https://www.eightathletics.com/products/sleep-mask-one"
-            className="inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ease-in-out text-sm"
-          >
-            Experience the future of sleep, today!
-          </a>
-          <div className="mt-8 text-sm">
-            <Link href="/" className="text-blue-600 hover:underline">
-              Go back to the homepage
-            </Link>
+          <div className="flex flex-col ">
+            <div className="md:h-full md:w-full w-64 h-64">
+              <Image
+                className="rounded-lg object-contain mx-auto shadow-black drop-shadow-sm "
+                src={"/images/image5.jpg"}
+                width="600"
+                height="600"
+                quality={100}
+                alt="Sleep Mask One from Eight Athletics"
+              />
+            </div>
+            <div className="py-4 ">
+              <div className="py-2">
+                <a
+                  href="https://www.eightathletics.com/products/sleep-mask-one"
+                  className="inline-block bg-blue-600 text-white py-2 px-8 rounded hover:bg-blue-700 transition duration-300 ease-in-out text-lg"
+                >
+                  Secure Yours Now 🌙
+                </a>
+              </div>
+              <div className=" text-xs">
+                <Link href="/" className="text-blue-600 hover:underline">
+                  Go back to the homepage
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
